f775128444f8835cd12c9143009089b7c619691e
sudhir-ssh <sudhir.ssh@gmail.com>
2022-12-26 23:06:12 +0530

    First Build

---
+++ Project3/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Veera.Sudhir
+ *
+ */
+module Project3 {
+}
---
+++ Project3/src/sample.java
@@ -0,0 +1,10 @@
+
+public class sample {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Jenkins Studio");
+		System.out.println("Sample Java Program");
+
+	}
+
+}
